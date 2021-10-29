8dff4a60dd9ae251886a512f46ab45486cdc18e1
Sebastian Nava <snaval1300@alumno.ipn.mx>
2021-10-29 00:01:43 -0500

    * Nueva dirección del perfil GATT * Descriptores de formato para los campos del servicio * Campo de apariencia como "Multisensor" (BT SIG)

--- main/nodo_ble.h
+++ main/nodo_ble.h
@@ -21,7 +21,6 @@
 #define GATTS_APP_ID                0x91                // ID escogido arbitrariamente
 #define PROFILE_NUM                 1
 #define NODO_SERVICE_ID             0
-#define MAX_SERVICE_UUID_LEN        16
 #define PROFILE_APP_IDX             0
 #define NODO_DEV_NAME               "IPN-NODO"
 #define SVC_INST_ID                 0
@@ -29,37 +28,48 @@
 #define ADV_CONFIG_FLAG             (1 << 0)
 #define SCAN_RSP_CONFIG_FLAG        (1 << 1)
 #define MTU_SIZE                    500
+#define DEV_BLE_APPEARANCE          0x556               // SIG Appearance Values (Multisensor)
 
 /* Definición del servicio */
 
+#define MAX_SERVICE_UUID_LEN        16
+#define PRES_FMT_LEN                7
 static uint8_t service_uuid[16] = {
-    /* LSB <--------------------------------------------------------------------------------> MSB */
+    // xxxxxxxx-0000-1000-8000-00805F9B34FB <-- Direción base
     //first uuid, 16bit, [12],[13] is the value
-    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xF1, 0x00, 0x00, 0x00,
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x31, 0x39, 0x39, 0x31
 };
 static const uint16_t primary_service_uuid = ESP_GATT_UUID_PRI_SERVICE;
 /* Valores asignados por Bluetooth SIG */
-static const uint16_t temperature_uuid = 0x2A6E;
-static const uint16_t humidity_uuid = 0x2B18; // Volts
-//static const uint16_t humidity_uuid= 0x2A6F; % de humedad
+static const uint16_t temperature_uuid = 0x2A6E;        // SIG GATT Characteristics and Object Type
+static const uint16_t humidity_uuid= 0x2A6F; 
 static const uint16_t lux_uuid = 0x2AFB;
 static const uint16_t generic_uuid = 0x2AF9;
+/* Characteristics formats (<<< LSB ----- MSB >>>) */
+static const uint8_t temp_presentation_format[7] =  {0x10, -1, 0x2f, 0x27, 0x01, 0x00, 0x00};
+static const uint8_t hum_presentation_format[7] =   {0x10, -3, 0x28, 0x27, 0x01, 0x00, 0x00};
+static const uint8_t lum_presentation_format[7] =   {0x10,  0, 0x31, 0x27, 0x01, 0x00, 0x00};
+/* Characteristics types UUIDs */
 static const uint16_t char_declaration_uuid = ESP_GATT_UUID_CHAR_DECLARE;
-static const uint16_t character_client_config_uuid = ESP_GATT_UUID_CHAR_CLIENT_CONFIG;
+static const uint16_t char_pres_fmt_uuid = ESP_GATT_UUID_CHAR_PRESENT_FORMAT;
+/* Characteristics props */
 static const uint8_t char_properties_rn = ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
+/* Characteristics values */
 static const int32_t init_value = 0;
-static const uint8_t temperature_ccc[2] = {0x00, 0x00};
 
 /* Enum de atributos para el perfil */
 enum {
     ID_SVC, 
     ID_CHAR_TEMP,
     ID_CHAR_TEMP_VAL,
-    ID_CHAR_TEMP_CCC,
+    ID_CHAR_TEMP_FMT,
     ID_CHAR_HUM,
     ID_CHAR_HUM_VAL,
+    ID_CHAR_HUM_FMT,
     ID_CHAR_LUM,
     ID_CHAR_LUM_VAL,
+    ID_CHAR_LUM_FMT,
     ID_CHAR_PH,
     ID_CHAR_PH_VAL,
     DB_LEN};
@@ -96,10 +106,16 @@ static const esp_gatts_attr_db_t gatt_db[DB_LEN] =  {
                 sizeof(int32_t),
                 (uint8_t *) &init_value 
             }},
-    /* Client Characteristic Configuration Descriptor */
-    [ID_CHAR_TEMP_CCC]  =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_client_config_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-      sizeof(uint16_t), sizeof(temperature_ccc), (uint8_t *)temperature_ccc}},
+    [ID_CHAR_TEMP_FMT] =
+        {{ESP_GATT_AUTO_RSP},
+            {
+                ESP_UUID_LEN_16,
+                (uint8_t *) &char_pres_fmt_uuid, 
+                ESP_GATT_PERM_READ,
+                PRES_FMT_LEN,
+                PRES_FMT_LEN,
+                (uint8_t *) &temp_presentation_format 
+            }},
     [ID_CHAR_HUM] =
         {{ESP_GATT_AUTO_RSP},
             {
@@ -120,6 +136,16 @@ static const esp_gatts_attr_db_t gatt_db[DB_LEN] =  {
                 sizeof(int32_t),
                 (uint8_t *) &init_value 
             }},
+    [ID_CHAR_HUM_FMT] =
+        {{ESP_GATT_AUTO_RSP},
+            {
+                ESP_UUID_LEN_16,
+                (uint8_t *) &char_pres_fmt_uuid, 
+                ESP_GATT_PERM_READ,
+                PRES_FMT_LEN,
+                PRES_FMT_LEN,
+                (uint8_t *) &hum_presentation_format 
+            }},
     [ID_CHAR_LUM] =
         {{ESP_GATT_AUTO_RSP},
             {
@@ -139,6 +165,16 @@ static const esp_gatts_attr_db_t gatt_db[DB_LEN] =  {
                 sizeof(int32_t),
                 sizeof(int32_t),
                 (uint8_t *) &init_value 
+            }},
+    [ID_CHAR_LUM_FMT] =
+        {{ESP_GATT_AUTO_RSP},
+            {
+                ESP_UUID_LEN_16,
+                (uint8_t *) &char_pres_fmt_uuid, 
+                ESP_GATT_PERM_READ,
+                PRES_FMT_LEN,
+                PRES_FMT_LEN,
+                (uint8_t *) &lum_presentation_format 
             }},
     [ID_CHAR_PH] =
         {{ESP_GATT_AUTO_RSP},
@@ -169,7 +205,7 @@ static esp_ble_adv_data_t adv_data = {
     .include_txpower     = true,
     .min_interval        = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
     .max_interval        = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
-    .appearance          = 0x00,
+    .appearance          = DEV_BLE_APPEARANCE,
     .manufacturer_len    = 0,    //TEST_MANUFACTURER_DATA_LEN,
     .p_manufacturer_data = NULL, //test_manufacturer,
     .service_data_len    = 0,
@@ -186,7 +222,7 @@ static esp_ble_adv_data_t scan_rsp_data = {
     .include_txpower     = true,
     .min_interval        = 0x0006,
     .max_interval        = 0x0010,
-    .appearance          = 0x00,
+    .appearance          = DEV_BLE_APPEARANCE,
     .manufacturer_len    = 0, //TEST_MANUFACTURER_DATA_LEN,
     .p_manufacturer_data = NULL, //&test_manufacturer[0],
     .service_data_len    = 0,
